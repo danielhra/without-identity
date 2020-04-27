50048b319ee9d048003a538e2e096687b17ca1d7
Daniel Rodríguez Alcalá <daniel.rodriguez@autentia.com>
2020-04-27 23:26:47 +0200

    cool changes

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,8 @@ import {Box, Heading, RoutedAnchor, Stack,} from "grommet";
 import img from "./assets/img/psychology-4440675.jpg";
 import styled from "styled-components";
 import Start from "./Start"
+import { Down } from 'grommet-icons';
+
 
 const StyledImage = styled.img`
     object-fit: fill;
@@ -20,9 +22,13 @@ const Main = () => (
 
         <Stack anchor={"bottom-left"}>
             <StyledImage src={img}/>
-            <Box height={"small"} margin={"medium"} align={"center"}>
+            <Box  margin={"large"}>
                 <Heading margin={"none"} size={"large"}>SIN IDENTIDAD</Heading>
             </Box>
+            <Box  width={"100vw"} pad={"medium"} align={"center"}>
+                <Down size={"large"}/>
+            </Box>
+
         </Stack>
 
         <Start/>
