d12c437c6c1e5eb0b56aa3faa8ea64e2446c86e1
Daniel Rodríguez Alcalá <daniel.rodriguez@autentia.com>
2020-04-28 23:02:20 +0200

    Deploy

---
+++ precache-manifest.dff4a8bead6b74126b82f378fe403a4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf49f7d4297e6cbedd0832508743ef6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1efee795c559d7e7cead",
+    "url": "./static/js/2.419b99a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.419b99a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "537b629f7c0c1ce0ddad",
+    "url": "./static/js/main.781b75d2.chunk.js"
+  },
+  {
+    "revision": "d61f1ba584a98a16217c",
+    "url": "./static/js/runtime-main.95d80b94.js"
+  },
+  {
+    "revision": "55c874a5e0eeab8f9f49be8ee45993da",
+    "url": "./static/media/Atenas-Sebastián.55c874a5.png"
+  },
+  {
+    "revision": "5c87be83438577809cda266c1b5bd586",
+    "url": "./static/media/Casablanca-Amal.5c87be83.png"
+  },
+  {
+    "revision": "b60ee64c832e2f8f33414d22a1c83fa7",
+    "url": "./static/media/Frases Cesár.b60ee64c.png"
+  },
+  {
+    "revision": "90183b19c226b04749425d9de888a017",
+    "url": "./static/media/Maleta-Abierta.90183b19.png"
+  },
+  {
+    "revision": "c959d4f9e689489d73f462b447078b76",
+    "url": "./static/media/Moscu-Uxue.c959d4f9.png"
+  },
+  {
+    "revision": "2d8778784b74b22a357006aefa1a72b9",
+    "url": "./static/media/Roma-Guiseppe.2d877878.png"
+  },
+  {
+    "revision": "f69d5df5858173eaa80ce4bf1f98f6aa",
+    "url": "./static/media/psychology-4440675.f69d5df5.jpg"
+  }
+]);
